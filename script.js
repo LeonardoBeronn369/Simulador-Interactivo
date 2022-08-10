3de50904b12464f660443a2ad54b31e1bf262d45
leonardo <leonardoberon33@gmail.com>
2022-08-09 22:00:17 -0300

    agregue arrays

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*
 function nuevaColeccion() {
     let prenda;
     do {
@@ -18,22 +18,21 @@ function nuevaColeccion() {
  
 function precios(prenda) {
     if (prenda === "jeans") {
-      return 500;
+      return 11000;
     } else if (prenda === "remera") {
-      return 600;
+      return 6000;
     } else {
-      return 1400;
+      return 9000;
     }
   }
    
 
 let cantidad;
-let precio = precios();
 let descuento;
 let compra;
 let pagar;
 
-function calcular(){
+function calcular(precio){
     cantidad = parseFloat(prompt("Ingresar cantidad"));
     compra = precio * cantidad;
     descuento = compra * 0.15
@@ -44,6 +43,44 @@ function calcular(){
 let productoSeleccionado = nuevaColeccion();
 let precioProd = precios(productoSeleccionado);
 
-calcular()
+calcular(precioProd);
+
+*/
 
 
+
+// Array
+
+const coleccion = ["jeans", "remera", "camisa"];
+
+
+function Prendas() {
+  for (let i = 0; i < coleccion.length; i++){
+    console.log(coleccion[i])
+  }
+} 
+
+function agregarPrendas(){
+  let nuevaPrenda = prompt("Agrega una prenda a tu colección: ");
+  let resultado = coleccion.includes(nuevaPrenda);
+  if(!resultado) {
+    coleccion.push(nuevaPrenda)
+  } else {
+    console.log("Ya tienes", nuevaPrenda, "elige  prenda!")
+  }
+}
+
+function quitarPrenda() {
+  let eliminaPrenda = prompt("Ingresa una prenda que no necesites");
+  let indice = coleccion.indexOf(eliminaPrenda)
+  if(indice !== -1) {
+    let resultado = coleccion.splice(indice, 1)
+    console.log("Ya no tienes esta prenda", resultado)
+  } else{
+    console.error("No se encuentra esa prenda en tu Coleccion", eliminaPrenda)
+  }
+}
+
+agregarPrendas()
+quitarPrenda()
+console.log(coleccion)
